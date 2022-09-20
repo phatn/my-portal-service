a0cddfcb1eb0b6b1abbbd654067cfc90efcc9a60
Phat Nguyen <phatnguyentanit@gmail.com>
2022-09-20 09:02:53 -0500

    Init commit

---
+++ src/main/java/com/example/myportalservice/MyPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.myportalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPortalServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myportalservice/MyPortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myportalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
